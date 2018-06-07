48553da1bd99f3c849c1a3b644500d4429fba35a
Lucas <almanza.marfrancisco@hotmail.com>
2018-06-07 11:02:34 -0500

    Lo que sigue es llamar a todos las tramas

--- main.c
+++ main.c
@@ -43,33 +43,74 @@ void printhex(const u_char *u){
 	printf("\n");
 }
 
-void printmac(char *d){
+void printbyte(char b){
+	unsigned char c;
+	for(c=128;c>0;c>>=1)
+		printf("%d",(b&c)?1:0);
+}
+
+void printmac(u_char *d){
 	for(int i=0;i<6;i++){
 			printf("%02x", d[i]);
 			if(i<5)
 				printf(":");
 		}
 }
+int loneth(u_char c[2],u_short *tam){
+	/*printf("\nprimer byte ");
+	printbyte(c[0]);
+	printf("\nsegundo byte ");
+	printbyte(c[1]);*/
+	u_short total=0;
+	total=c[0];
+	total<<=8;
+	total=total|c[1];
+	// printf("\ntam: %hu",total);
+	*tam=total;
+	if(total<=1500)//**Compara para saber si el campo es menor o igual a 1500
+		return 0;
+	else
+		return 1;
+}
 
-void ethernet_header(const u_char *t){
+
+int examen(const u_char trama[]/*,int longitud, char resultado[],int longres*/){
+	u_short ethertype=0;
+	printf("Funcion examen\n");
 	//cabecera ethernet
 	u_char MAC_DEST[6];
 	u_char MAC_SOURCE[6];
 	u_char TYPE[2];
 	printf("Trama:\n");
-	printhex(t);
-	memcpy(MAC_DEST,&t[0],6);
-	memcpy(MAC_SOURCE,&t[6],6);
+	printhex(trama);
+	memcpy(MAC_DEST,&trama[0],6);
+	memcpy(MAC_SOURCE,&trama[6],6);
+	memcpy(TYPE,&trama[12],2);
 	printf("\n\nMac destino: ");
 	printmac(MAC_DEST);
 	printf("\nMac origen: ");
 	printmac(MAC_SOURCE);
-	printf("\nTipo: %02x %02x \n");
-}
-
-int examen(const u_char trama[]/*,int longitud, char resultado[],int longres*/){
-	printf("Funcion examen\n");
-	ethernet_header(trama);	
+	printf("\nTipo: %02x %02x\n", TYPE[0],TYPE[1]);
+	//Aqui se ve si es ethertype o longitud
+	// printf("\n y es: %s",loneth(TYPE)?"tipo":"longitud");
+	if(loneth(TYPE,&ethertype)){
+		printf("\nTrama ethertype");
+		switch(ethertype){
+			case 2034:
+				printf("\nLa trama es de IP");
+				break;
+		}
+	}
+	else{
+		printf("\nTrama LLC");
+		u_char DSAP=trama[14],SSAP=trama[15];
+		printf("\nDSAP: %02x",DSAP);
+		printf("\nSSAP: %02x",SSAP);
+		//Para REJ
+		if((trama[16]&1))
+			if(trama[16]==0x09)
+				printf("\n LLC REJ");
+	}
 	return 0;
 }
 
